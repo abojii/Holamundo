d2352178b5a9e83b0a696a0a6936551065402692
abojii <abojorgec@gmail.com>
2024-09-22 16:51:56 -0600

    Formulario y codigo del ejercicio numero 1

---
+++ ejercicio1/ejercicio1/ejercicio1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ejercicio1
+{
+    partial class ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.lblOperacion = new System.Windows.Forms.Label();
+            this.cmbOperacion = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Location = new System.Drawing.Point(29, 52);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(251, 20);
+            this.lblNumero1.TabIndex = 0;
+            this.lblNumero1.Text = "Ingrese el valor del primer numero:";
+            this.lblNumero1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(29, 113);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(269, 20);
+            this.lblNumero2.TabIndex = 1;
+            this.lblNumero2.Text = "Ingrese el valor del segundo numero:";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(330, 46);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 26);
+            this.txtNumero1.TabIndex = 2;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(330, 110);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 26);
+            this.txtNumero2.TabIndex = 3;
+            // 
+            // lblOperacion
+            // 
+            this.lblOperacion.AutoSize = true;
+            this.lblOperacion.Location = new System.Drawing.Point(31, 173);
+            this.lblOperacion.Name = "lblOperacion";
+            this.lblOperacion.Size = new System.Drawing.Size(249, 20);
+            this.lblOperacion.TabIndex = 4;
+            this.lblOperacion.Text = "Seleccione la operacion a realizar:";
+            // 
+            // cmbOperacion
+            // 
+            this.cmbOperacion.FormattingEnabled = true;
+            this.cmbOperacion.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "x",
+            "/"});
+            this.cmbOperacion.Location = new System.Drawing.Point(330, 170);
+            this.cmbOperacion.Name = "cmbOperacion";
+            this.cmbOperacion.Size = new System.Drawing.Size(104, 28);
+            this.cmbOperacion.TabIndex = 6;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(64, 251);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(135, 33);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(31, 323);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(86, 20);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(275, 251);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(121, 33);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 197);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(9, 305);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(444, 70);
+            this.panel2.TabIndex = 11;
+            // 
+            // ejercicio1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 387);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.cmbOperacion);
+            this.Controls.Add(this.lblOperacion);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "ejercicio1";
+            this.Text = "Ejercicio1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label lblOperacion;
+        private System.Windows.Forms.ComboBox cmbOperacion;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ ejercicio1/ejercicio1/ejercicio1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio1
+{
+    public partial class ejercicio1 : Form
+    {
+        public ejercicio1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumero1.Text, out double numero1) && double.TryParse(txtNumero2.Text, out double numero2))
+            {
+                //MessageBox.Show("Los textBox tienen un numero");
+                string operacion = cmbOperacion.SelectedItem.ToString();
+                double resultado = 0;
+                bool operacionValida = true;
+
+                switch (operacion)
+                {
+                    case "+":
+                        resultado = numero1 + numero2;
+                        break;
+                    case "-":
+                        resultado = numero1 - numero2;
+                        break;
+                    case "*":
+                        resultado = numero1 * numero2;
+                        break;
+                    case "/":
+                        if (numero2 != 0)
+                        {
+                            resultado = numero1 / numero2;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error: no se puede dividir por cero");
+                            operacionValida = false;
+                            txtNumero2.Text = string.Empty;
+                            txtNumero2.Focus();
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("Seleccione una operacion valida");
+                        operacionValida = false;
+                        cmbOperacion.Focus();
+                        break;
+
+                }
+
+                if (operacionValida)
+                {
+                    lblResultado.Text = "Resultado: " + resultado.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, introduce numeros validos");
+                txtNumero1.Text = string.Empty;
+                txtNumero2.Text = string.Empty;
+                txtNumero1.Focus();
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = string.Empty;
+            txtNumero2.Text = string.Empty;
+            lblResultado.Text = "Resultado: ";
+            txtNumero1.Focus();
+        }
+    }
+}
